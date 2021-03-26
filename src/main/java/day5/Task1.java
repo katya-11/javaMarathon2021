0173545b160e29a44b4ccdebe31c7faa78fe6050
Katya Makeychik <katya.makeychik11@gmail.com>
2021-03-26 19:20:00 +0100

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,69 @@
 package day5;
 
-public class Task1 {
-    public static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        class Car {
+            private int year;
+            private String color;
+            private String model;
+
+            public void setYear(int year) {
+                if (year <= 0) {
+                    System.out.println("Введи корректный год");
+                } else {
+                    this.year = year;
+                }
+            }
+
+            public int getYear() {
+                return year;
+            }
+
+            public void setColor(String color) {
+                if (color.isEmpty()) {
+                    System.out.println("Ты ввел пустой цвет");
+                } else {
+                    this.color = color;
+                }
+            }
+
+            public String getColor() {
+                return color;
+            }
+
+            public void setModel(String model) {
+                if (model.isEmpty()) {
+                    System.out.println("Ты ввел пустую модель");
+                } else {
+                    this.model = model;
+                }
+            }
+
+            public String getModel() {
+                return model;
+            }
+        }
+
+        Car car1 = new Car();
+        System.out.println("Введи год выпуска авто");
+        car1.setYear(Integer.parseInt(reader.readLine()));
+
+        System.out.println("Введи модель авто");
+        car1.setModel(reader.readLine());
+
+        System.out.println("Введи цвет авто");
+        car1.setColor(reader.readLine());
+
+        System.out.println("Цвет авто: " + car1.getColor());
+        System.out.println("Год выпуска авто: " + car1.getYear());
+        System.out.println("Модель авто: " + car1.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,41 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        class Motobike {
+            private int year;
+            private String model;
+            private String color;
+
+            public Motobike(int year, String model, String color) {
+                this.year = year;
+                this.model = model;
+                this.color = color;
+            }
+
+            public Motobike(int year, String model) {
+                this.year = year;
+                this.model = model;
+                this.color = "Цвет не задан";
+            }
+
+            public int getYear() {
+                return year;
+            }
+
+            public String getColor() {
+                return color;
+            }
+
+            public String getModel() {
+                return model;
+            }
+        }
+
+        Motobike motobike1 = new Motobike(2020, "BMW");
+        Motobike motobike2 = new Motobike(2010, "Yamaha", "Red");
+
+        System.out.println("Год выпуска: " + motobike1.getYear() + ", Модель: " + motobike1.getModel() + ", Цвет: " + motobike1.getColor());
+        System.out.println("Год выпуска: " + motobike2.getYear() + ", Модель: " + motobike2.getModel() + ", Цвет: " + motobike2.getColor());
+
     }
 }
