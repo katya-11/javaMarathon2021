5aebb88bcd74de129d975e03e63fc26df5ad365e
Katya Makeychik <katya.makeychik11@gmail.com>
2021-04-17 22:30:22 +0200

    Day 15, completed tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,36 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file1 = new File("src/main/resources/shoes.csv");
+        File file2 = new File("src/main/resources/missing_shoes.txt");
+        PrintWriter printWriter = null;
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file1);
+            printWriter = new PrintWriter(file2);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] shoesData = line.split(";");
+                if (Integer.parseInt(shoesData[2]) == 0) {
+                    printWriter.println(line);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } finally {
+            if (printWriter != null) {
+                printWriter.close();
+                scanner.close();
+            }
+        }
 
     }
 }
